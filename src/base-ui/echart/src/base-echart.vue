3e7a5cb314eea9d2aca9893f32988e4fa27e3b70
vgri <vgri199712@163.com>
2022-09-25 18:02:06 +0800

    feat(base-echart): option变化时候重新渲染图表

--- src/base-ui/echart/src/base-echart.vue
+++ src/base-ui/echart/src/base-echart.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, withDefaults, defineProps } from 'vue'
+import { ref, onMounted, withDefaults, defineProps, watchEffect } from 'vue'
 import { EChartsOption } from 'echarts'
 import { useEcharts } from '../hooks/useEcharts'
 import { vgriEmitter } from '@/utils/event'
@@ -30,6 +30,11 @@ onMounted(() => {
   // 侧边栏收缩/扩张时，重新计算尺寸
   vgriEmitter.on('echartResize', () => {
     updateSize()
+  })
+
+  // 收集option依赖，在option发生变化的时候，重新渲染图表
+  watchEffect(() => {
+    setOptions(props.option)
   })
 })
 </script>
